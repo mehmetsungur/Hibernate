8acd28a211b1ede1470a4b99656a7794f0977199
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-05 23:43:33 +0300

    update

---
+++ src/main/java/com/modulcar/hb01/annotation/student01.java
@@ -0,0 +1,51 @@
+package com.modulcar.hb01.annotation;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_student01")
+public class student01 {
+
+    @Id
+    private int id;
+
+    @Column(name = "std_name",length = 100,nullable = false,unique = true)
+    private String name;
+    private int grade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "student01{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
