17eba457278b4b67ce5d00a014144de7796ebd33
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-12 18:24:35 +0300

    update

--- src/main/java/com/modulcar/recap/bi_onetoone/Diary04.java
+++ src/main/java/com/modulcar/recap/bi_onetoone/Diary04.java
@@ -1,2 +1,53 @@
-package com.modulcar.recap.bi_onetoone;public class Diary04 {
+package com.modulcar.recap.bi_onetoone;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Diary04 {
+
+    @Id//PK
+    private int id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "diary")
+    //@JoinColumn(name = "std_id",unique = true)//opsiyonel
+    private Student04 student;//1001
+
+    //getter-setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student04 getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student04 student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Diary{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                /* ", student=" + student +*/
+                '}';
+    }
 }
